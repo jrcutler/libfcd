cef7c2b570e02ae51acf7790b5f924402eedf532
Justin R. Cutler <justin.r.cutler@gmail.com>
2012-03-02 18:07:14 -0500

    Added private FUNcube dongle get/set functions.

--- fcd.c
+++ fcd.c
@@ -6,6 +6,7 @@
 #endif
 
 #include <stdlib.h> /* NULL, malloc, free */
+#include <string.h> /* memset, memcpy */
 #include "hidapi.h" /* hid_* */
 #include "fcd.h" /* FCD */
 
@@ -21,6 +22,66 @@ struct FCD_impl
 	/*! \brief HID device */
 	hid_device *hid_dev;
 };
+
+
+/*
+ * Private Functions
+ */
+
+
+int fcd_get(FCD *dev, unsigned char cmd, void *data, unsigned char len)
+{
+	unsigned char buffer[65];
+	int result = -1;
+
+	/*! \TODO validate cmd */
+	if (len && (NULL == data)) return -1;
+	if (len > 62) return -1;
+
+	memset(buffer, 0, sizeof(buffer));
+	buffer[1] = cmd;
+	if (hid_write(dev->hid_dev, buffer, 2) == 2)
+	{
+		buffer[1] = 0;
+		if (hid_read(dev->hid_dev, buffer, len+2) == len+2)
+		{
+			if ((buffer[0] == cmd) && (buffer[1] == 1))
+			{
+				memcpy(data, buffer+2, len);
+				result = len;
+			}
+		}
+	}
+
+	return result;
+}
+
+
+int fcd_set(FCD *dev, unsigned char cmd, const void *data, unsigned char len)
+{
+	unsigned char buffer[65];
+	int result = -1;
+
+	/*! \TODO validate cmd */
+	if (len && (NULL == data)) return -1;
+	if (len > 62) return -1;
+
+	buffer[0] = 0;
+	buffer[1] = cmd;
+	memcpy(buffer+2, data, len);
+	if (hid_write(dev->hid_dev, buffer, len+2) == len+2)
+	{
+		if (hid_read(dev->hid_dev, buffer, 2) == 2)
+		{
+			if ((buffer[0] == cmd) && (buffer[1] == 1))
+			{
+				result = len;
+			}
+		}
+	}
+
+	return result;
+}
 
 
 /*
